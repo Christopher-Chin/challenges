7abe754f7bf0161c822f07f3dfa69b5deedaab33
Christopher Chin <christopher.phil.chin@gmail.com>
2019-05-09 21:38:10 +1000

    Create compare-es6.js

---
+++ compare-es6.js
@@ -0,0 +1,17 @@
+/*
+
+  (ES6) Write a JavaScript program to compare two numbers to determine if the 
+  first one contains equivalent property values to the second one.
+
+*/
+
+let result = document.querySelector('#result');
+
+const equals = (a,b) =>{
+  if(a == b){
+    result.innerHTML = `<strong>Result 1:</strong>${a}<br/>
+												<strong>Result 2:</strong> ${b} <br/> Both values are equal!`;
+  }
+}
+
+equals(10,10);
